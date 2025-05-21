e1d24ded42c644b2d31032c173f8a20ec5990a79
Rafik Rahmi <rafikrahmi10@gmail.com>
2025-05-21 10:32:45 +0100

    add custumized

--- src/utils/testSchema.ts
+++ src/utils/testSchema.ts
@@ -10,3 +10,14 @@ export const testValue = (schema: z.ZodSchema, value: any) => {
         return false;
     }
 };
+
+export const testAsyncValue = async (schema: z.ZodSchema, value: any) => {
+    try {
+        const result = await schema.parseAsync(value);
+        console.log(`Value: ${value}, Result: ${result}`);
+        return true;
+    } catch (e) {
+        console.error(`Value: ${value}, Error: ${e}`);
+        return false;
+    }
+};
